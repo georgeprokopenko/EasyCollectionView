9af045d704a0e8c0896597e65e7cb8e771758d1d
George Prokopenko <prokopenko@simple.life>
2024-04-12 09:52:13 +0300

    Update Pixel.swift

--- Sources/EasyCollectionView/Extensions/Pixel.swift
+++ Sources/EasyCollectionView/Extensions/Pixel.swift
@@ -1,23 +1,23 @@
 import UIKit
 import Foundation
 
-struct Pixel {
+public struct Pixel {
 
-    static var size: CGFloat = {
+    public static var size: CGFloat = {
         1.0 / UIScreen.main.scale
     }()
 
-    static func roundToPixel(_ value: CGFloat) -> CGFloat {
+    public static func roundToPixel(_ value: CGFloat) -> CGFloat {
         let screenScale = UIScreen.main.scale
         return round(value * screenScale) / screenScale
     }
 
-    static func floorToPixel(_ value: CGFloat) -> CGFloat {
+    public static func floorToPixel(_ value: CGFloat) -> CGFloat {
         let screenScale = UIScreen.main.scale
         return floor(value * screenScale) / screenScale
     }
 
-    static func ceilToPixel(_ value: CGFloat) -> CGFloat {
+    public static func ceilToPixel(_ value: CGFloat) -> CGFloat {
         let screenScale = UIScreen.main.scale
         return ceil(value * screenScale) / screenScale
     }
